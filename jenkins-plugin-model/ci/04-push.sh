3aaffa0ea2cefbf3b4445e53dec2bd8d3e42b9ab
vedansh13-stack <vedanshdhomane13@gmail.com>
2026-05-05 15:11:15 +0530

    Change Docker image tag from swapnilwaghmare to vedansh13

--- jenkins-plugin-model/ci/04-push.sh
+++ jenkins-plugin-model/ci/04-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker image build -t swapnilwaghmare/jenkins-demo2:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
+docker image build -t vedansh13/jenkins-demo2:$1 -f jenkins-plugin-model/src/demo2-publish/dockerfile .
 
 if [ -z ${DOCKER_HUB_USER+x} ]
 then 
